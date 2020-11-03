ce0238d2f9565dc6be51b28d8fdad12ddb934a59
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-03 12:20:11 +0300

    Added users class

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    
+
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.username}'
