b25fdf88b2ef9f8e4cd3b95313b6416eb9550560
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-03 13:02:11 +0300

    Added the views.py file structure

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for, flash, redirect, abort, request
+from . import main
+from .forms import RegistrationForm, LoginForm
+from ..models import User
+from .. import db
+from flask_login import login_user,login_required, logout_user, current_user
+
+@main.route('/')
+def home():
+    return render_template('index.html')
