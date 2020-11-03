5be5e2ff69c4e348ae85eb43a21eaca55048b00b
andyjohn23 <andrewnick906@gmail.com>
2020-11-04 01:10:41 +0300

    updated files

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect, abort, request
 from . import main
-from .forms import RegistrationForm, LoginForm
 from ..models import User
 from .. import db
 from flask_login import login_user,login_required, logout_user, current_user
 
 @main.route('/')
-def home():
+def index():
+    """
+    view root page function that returns the index page and its data
+    """
     return render_template('index.html')
