3ac9a94a3ce534e44dcb8cb63e23fd9938657d3a
joansaj <joannsaj@gmail.com>
2020-11-04 15:34:19 +0300

    added displays for forms

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,50 @@
 from flask import Flask, render_template, url_for, flash, redirect, abort, request
 from . import main
-# from .forms import RegistrationForm, LoginForm
 from ..models import User
-from .. import db
-from flask_login import login_required #, logout_user, current_user
+from .. import db, photos
+from flask_login import login_required , logout_user, current_user
+from .forms import UpdateProfile
 
 @main.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+    
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))        
